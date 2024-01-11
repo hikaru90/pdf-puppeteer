cb83bbf772ff1558d17ba6db347a9af7bcbeb6ef
hikaru90 <alexbueckner@gmail.com>
2024-01-11 15:21:25 +0100

    add dockerfile

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = 3000;
 
 app.get('/screenshot', async (req, res) => {
   // Launch a headless browser
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new"
+  });
 
   // Create a new page
   const page = await browser.newPage();
